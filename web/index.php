6cbc3b2b653ee94a41ac58af66ce9d82350ca0ed
ASBQuirino <asbquirino@yahoo.com.br>
2016-12-01 23:59:41 -0200

    Verificando se o Silex esta funcionando - Olá mundo

--- web/index.php
+++ web/index.php
@@ -3,5 +3,10 @@
 require_once __DIR__."/../vendor/autoload.php";
 
 $app = new Silex\Application();
+$app['debud'] = true;
+
+$app->get('/hello', function() {
+    return 'Ola mundo!';
+});
 
 $app->run();
